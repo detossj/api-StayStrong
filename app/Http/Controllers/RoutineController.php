372315885640a14fc98fb819288edc0b32fbb238
detossj <pc0216@LabFI.ucsc.cl>
2025-06-17 13:36:30 -0400

    added functions

---
+++ app/Http/Controllers/RoutineController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoutineController extends Controller
+{
+     //METODO POST
+
+    /**
+     * Crea una nueva rutina para el usuario autenticado
+     * @authenticated
+     * @header Authorization Bearer {token}
+     * @bodyParam title string required
+     * @bodyParam icon integer optional
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'user_id' => 'required|integer|exists:users,id',
+            'date' => 'required|date',
+
+        ]);
+
+        $routine = $request->user()->routines()->create($validated);
+        return response()->json($routine, 201);
+    }
+
+    //METODO GET
+
+    /**
+     * Muestra una rutina específica del usuario
+     * @authenticated
+     * @header Authorization Bearer {token}
+     */
+    public function show(Request $request, $id)
+    {
+        $routine = $request->user()->routines()->find($id);
+        if (!$routine) {
+            return response()->json(['message' => 'Rutina no encontrada'], 404);
+        }
+
+        return response()->json($routine);
+    }
+
+
+
+}
