b8a016dff080091a2d14eeaa2d2c164eaffe6bab
detossj <jorgerubilarssj@gmail.com>
2025-06-17 15:00:00 -0400

    detossj
    added import

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 class AuthController extends Controller
 {
     /**
--- app/Http/Controllers/RoutineController.php
+++ app/Http/Controllers/RoutineController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Routine;
 
 class RoutineController extends Controller
 {
