9e7b09f0e2a13699e97ba7aff4bd155302653247
deto <jorgerubilarssj@gmail.com>
2025-06-30 15:19:47 -0400

    Update DatabaseSeeder.php
    added RecipeSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(RoutineVideoSeeder::class);
 
+        $this->call(RecipeSeeder::class);
+
 
         User::factory()->create([
             'name' => 'Test User',
